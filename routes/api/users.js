366a2d4ae8e16b88e13f67cd229d2957693b23b5
reet-571099 <63258652+reet-571099@users.noreply.github.com>
2021-03-03 12:45:51 +0530

    JWT sign and verify middleware

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 // @route    POST api/users
 // @desc     Register user
@@ -55,9 +57,25 @@ router.post(
         user.password = await bcrypt.hash(password, salt);
   
         await user.save();
+
+        const payload = {
+            user: {
+              id: user.id
+            }
+          };
+
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+              }
+            );
   
         // Return jsonwebtoken
-        res.send('User registered');
+        // res.send('User registered');
       } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error');
